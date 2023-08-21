1020e64a959914fbccc65901bf4f4499d3055b1a
yungstarry <111007711+yungstarry@users.noreply.github.com>
2023-08-21 22:59:48 +0100

    first commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://authzzz-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://authzzzzz-app.vercel.app"],
     credentials: true,
   })
 );
